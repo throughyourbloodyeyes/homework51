3a59ed1f752ea6d1bc92b6c1c9aed755f1db8cef
throughyourbloodyeyes <133150105+throughyourbloodyeyes@users.noreply.github.com>
2023-05-18 12:36:36 +0600

    homework51

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import Ball from './Balls/Balls';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [numbers, setNumbers] = useState<number[]>([]);
+
+  const generateNumbers = () => {
+    const newNumbers: number[] = [];
+    while (newNumbers.length < 5) {
+      const randomNumber = Math.floor(Math.random()*32) + 5;
+
+      if (!newNumbers.includes(randomNumber)){
+        newNumbers.push(randomNumber);
+      }
+    }
+    setNumbers(newNumbers.sort((a, b) => a - b))
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={generateNumbers} className='button1'>
+        New numbers
+      </button>
+      <div className = "balls-container">
+        {numbers.map((number, index) => (
+          <Ball key={index} number={number} />
+        ))}
+      </div>
     </div>
   );
 }
